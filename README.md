4f08bc50142cd6871a20ed65d4e8d703e89b9b32
Burak Mestan <burakmestan@gmail.com>
2019-07-19 21:17:04 +0300

    fixes

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Simplest React Setuo For A HTML Project
+# Simplest React Setup
 
-Compile: 
+React and react-dom are imported from CDN.
+Just transpile and use anywhere. 
+
+Transpile: 
 npm run build
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+function Greet() {
+  const greeting = "Hello, React!";
 
+  return <h1>{greeting}</h1>;
+}
 
-const domContainer = document.querySelector('.react');
-ReactDOM.render( <h1>HELLO REACT!</h1> , domContainer);
+const domContainer = document.querySelector(".react");
+ReactDOM.render(
+  <div>
+    <Greet />
+  </div>,
+  domContainer
+);
