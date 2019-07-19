317ceb643ff1832a8c3d13a9be6e698765211c60
Burak Mestan <burakmestan@gmail.com>
2019-07-19 19:15:35 +0300

    simple setup ready

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simplest React Setuo For A HTML Project
+
+Compile: 
+npm run build
---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+
+const domContainer = document.querySelector('.react');
+ReactDOM.render( <h1>HELLO REACT!</h1> , domContainer);
